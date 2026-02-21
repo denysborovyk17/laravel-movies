da8e0818b4ee717abfb4443ed21030188c800ae9
Denys Borovyk <borovikdenis748@gmail.com>
2026-02-21 19:09:19 +0200

    Remove unused methods

--- app/Policies/MoviePolicy.php
+++ app/Policies/MoviePolicy.php
@@ -8,16 +8,6 @@ use Illuminate\Auth\Access\Response;
 
 class MoviePolicy
 {
-    public function viewAny(User $user): bool
-    {
-        return true;
-    }
-
-    public function view(User $user, Movie $movie): bool
-    {
-        return true;
-    }
-
     public function create(User $user): bool
     {
         return $user->isAdmin();
@@ -31,23 +21,5 @@ class MoviePolicy
     public function delete(User $user, Movie $movie): bool
     {
         return $user->isAdmin();
-    }
-
-    public function restore(User $user, Movie $movie): bool
-    {
-        return $user->isAdmin();
-    }
-
-    public function forceDelete(User $user, Movie $movie): bool
-    {
-        return $user->isAdmin();
-    }
-
-    public function updateField(User $user, Movie $movie,  string $field): bool
-    {
-        return match ($field) {
-            'director_id', 'title', 'image', 'slug', 'description', 'year', 'genre', 'rating', 'status' => $user->isAdmin(),
-            default => false
-        };
     }
 }
