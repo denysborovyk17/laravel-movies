8789c0ac0f6cf263574cae628a60e40fb932cd0d
Denys Borovyk <borovikdenis748@gmail.com>
2026-02-21 19:05:40 +0200

    Connect MovieSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
-            DirectorSeeder::class,
+            // DirectorSeeder::class,
             MovieSeeder::class
         ]);
     }
