99725f22b69d69b60f3ead4328c5de7e7568c591
Denys Borovyk <borovikdenis748@gmail.com>
2026-02-21 19:01:33 +0200

    Add factory for model Movie

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
+ */
+class MovieFactory extends Factory
+{
+    public function definition(): array
+    {
+        $title = $this->faker->sentence(mt_rand(5, 8));
+
+        $slugBase = Str::slug($title);
+        $slug = $slugBase . "-" . $this->faker->unique()->numberBetween(1000, 999999);
+
+        return [
+            'title' => $title,
+            'slug' => $slug,
+            'description' => $this->faker->paragraph(mt_rand(2, 4)),
+            'year' => $this->faker->year(2050),
+            'genre' => $this->faker->dateTimeBetween('-30days', 'now'),
+            'rating' => $this->faker->numberBetween(1, 10)
+        ];
+    }
+}
