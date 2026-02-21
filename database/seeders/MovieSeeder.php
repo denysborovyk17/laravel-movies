f0509542dae514b0ffa7700bab50f231051b4639
Denys Borovyk <borovikdenis748@gmail.com>
2026-02-21 19:04:51 +0200

    Fix seeder for fake data

--- database/seeders/MovieSeeder.php
+++ database/seeders/MovieSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Director;
 use App\Models\Movie;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class MovieSeeder extends Seeder
@@ -14,27 +12,6 @@ class MovieSeeder extends Seeder
      */
     public function run(): void
     {
-        $nolan = Director::where('name', 'Christopher Nolan')->first();
-        $spielberg = Director::where('name', 'Steven Spielberg')->first();
-
-        Movie::create([
-            'title' => 'Inception',
-            'slug' => 'inception',
-            'description' => 'Dream within a dream...',
-            'year' => 2010,
-            'genre' => 'Sci-Fi',
-            'rating' => 8.8,
-            'director_id' => $nolan->id,
-        ]);
-
-        Movie::create([
-            'title' => 'Jurassic Park',
-            'slug' => 'jurassic-park',
-            'description' => 'Dinosaurs come to life!',
-            'year' => 1993,
-            'genre' => 'Adventure',
-            'rating' => 8.1,
-            'director_id' => $spielberg->id,
-        ]);
+        Movie::factory(30)->create();
     }
 }
