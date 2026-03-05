4c1a4fafc6a83a4ae39fa1427540c99465abea2a
Denys Borovyk <borovikdenis748@gmail.com>
2026-03-05 10:06:08 +0200

    Add ide-helper phpDocBlocks to models

--- app/Models/Director.php
+++ app/Models/Director.php
@@ -4,6 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Movie> $movies
+ * @property-read int|null $movies_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Director newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Director newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Director query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Director whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Director whereName($value)
+ * @mixin \Eloquent
+ */
 class Director extends Model
 {
     protected $fillable = ['name'];
--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -7,6 +7,41 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @property int $id
+ * @property string $title
+ * @property int|null $director_id
+ * @property string|null $image
+ * @property string|null $slug
+ * @property string|null $description
+ * @property string|null $year
+ * @property string|null $genre
+ * @property float|null $rating
+ * @property MovieStatus $status
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read \App\Models\Director|null $director
+ * @property-read string|null $image_url
+ * @method static \Database\Factories\MovieFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie published()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie whereDirectorId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie whereGenre($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie whereRating($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie whereYear($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie withTrashed(bool $withTrashed = true)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Movie withoutTrashed()
+ * @mixin \Eloquent
+ */
 class Movie extends Model
 {
     use HasFactory, SoftDeletes;
