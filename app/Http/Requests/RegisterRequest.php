62cdb47e8833aca886c649d685c8627aed3a66cc
Denys Borovyk <borovikdenis748@gmail.com>
2026-03-13 12:27:30 +0200

    add bail rule to email field

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -25,8 +25,8 @@ class RegisterRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email',
-            'password' => 'required|string|min:8',
+            'email' => 'bail|required|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
         ];
     }
 
