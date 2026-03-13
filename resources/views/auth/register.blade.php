ee7c2afe678da340df0e22cab32f3ce1a67e2468
Denys Borovyk <borovikdenis748@gmail.com>
2026-03-14 00:01:54 +0200

    format code

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,13 +3,13 @@
 @section('title', 'Register Page')
 
 @section('content')
-    <h1>Sign up</h1>
-    @include('errors.form-errors')
-    <form method="POST" action="{{ route('register.store') }}">
-        @csrf
-        <input type="text" name="name" placeholder="Name">
-        <input type="email" name="email" placeholder="Email">
-        <input type="password" name="password" placeholder="Password">
-        <button type="submit">Register</button>
-    </form>
+<h1>Sign up</h1>
+@include('errors.form-errors')
+<form method="POST" action="{{ route('register.store') }}">
+    @csrf
+    <input type="text" name="name" placeholder="Name">
+    <input type="email" name="email" placeholder="Email">
+    <input type="password" name="password" placeholder="Password">
+    <button type="submit">Register</button>
+</form>
 @endsection
