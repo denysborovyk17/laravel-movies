a852d5482376409da33b336ec9d62d20562ae33c
Denys Borovyk <borovikdenis748@gmail.com>
2026-03-13 23:59:20 +0200

    feat(dto): add UserDTO for registration

---
+++ app/DTOs/UserDTO.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DTOs;
+
+class UserDTO
+{
+    public function __construct(
+        public readonly string $name,
+        public readonly string $email,
+        public readonly string $password
+    ) {}
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            name: $data['name'],
+            email: $data['email'],
+            password: $data['password']
+        );
+    }
+}
