8a6dc9c39f858e9a0d3a49990f3a472925f2ef35
Denys Borovyk <borovikdenis748@gmail.com>
2026-03-18 20:19:13 +0200

    add AuthDTO, LoginDTO, RegisterDTO, and UserViewDTO for input/output separation

---
+++ app/DTOs/LoginDTO.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DTOs;
+
+class LoginDTO
+{
+    public function __construct(
+        public readonly string $email,
+        public readonly string $password
+    ) {}
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            email: $data['email'],
+            password: $data['password']
+        );
+    }
+}
