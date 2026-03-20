2c4cbd3dc1dd7c3f3a053168a8bef31c9b6e1b70
Denys Borovyk <borovikdenis748@gmail.com>
2026-03-20 10:46:01 +0200

    feat(movie): add helper methods to MovieStatus enum

--- app/Enums/MovieStatus.php
+++ app/Enums/MovieStatus.php
@@ -9,4 +9,19 @@ enum MovieStatus: string
     case DRAFT = 'draft';
     case ARCHIVED = 'archived';
     case PUBLISHED = 'published';
+
+    public function isDraft(): bool
+    {
+        return $this === self::DRAFT;
+    }
+
+    public function isArchived(): bool
+    {
+        return $this === self::ARCHIVED;
+    }
+
+    public function isPublished(): bool
+    {
+        return $this === self::PUBLISHED;
+    }
 }
