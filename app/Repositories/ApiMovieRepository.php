c4efba4757bfc8f2060fc6e8c3671962f915a74e
Denys Borovyk <borovikdenis748@gmail.com>
2026-03-21 20:18:01 +0200

    add ApiMovieRepository for ApiMovieService

---
+++ app/Repositories/ApiMovieRepository.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace App\Repositories;
+
+class ApiMovieRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
