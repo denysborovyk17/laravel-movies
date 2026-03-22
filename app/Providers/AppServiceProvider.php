bcbf5b5acb05f561af1641770b8cc72457058955
Denys Borovyk <borovikdenis748@gmail.com>
2026-03-22 18:12:48 +0200

    connect new Services and Repositories with their Interfaces

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,13 @@
 
 namespace App\Providers;
 
+use App\Repositories\ApiAuthRepository;
+use App\Repositories\ApiMovieRepository;
+use App\Repositories\Interfaces\{ApiMovieRepositoryInterface, ApiAuthRepositoryInterface};
 use App\Repositories\Interfaces\MovieRepositoryInterface;
 use App\Repositories\MovieRepository;
-use App\Services\{ApiMovieService, MovieService};
-use App\Services\Interfaces\{ApiMovieServiceInterface, MovieServiceInterface};
+use App\Services\{ApiAuthService, ApiMovieService, MovieService};
+use App\Services\Interfaces\{ApiAuthServiceInterface, ApiMovieServiceInterface, MovieServiceInterface};
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,9 +17,13 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(MovieRepositoryInterface::class, MovieRepository::class);
-
-        $this->app->bind(MovieServiceInterface::class, MovieService::class);
         $this->app->bind(ApiMovieServiceInterface::class, ApiMovieService::class);
+
+        $this->app->bind(ApiMovieRepositoryInterface::class, ApiMovieRepository::class);
+        $this->app->bind(MovieServiceInterface::class, MovieService::class);
+        
+        $this->app->bind(ApiAuthRepositoryInterface::class, ApiAuthRepository::class);
+        $this->app->bind(ApiAuthServiceInterface::class, ApiAuthService::class);
     }
 
     public function boot(): void
