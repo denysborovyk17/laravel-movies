6079faad13f97a0c2a9887ea58597dafc49cab9c
Denys Borovyk <borovikdenis748@gmail.com>
2026-03-23 13:24:37 +0200

    rename DTO file with suffix Dto

---
+++ app/DTO/RegisterDto.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace App\DTO;
+
+class RegisterDto
+{
+    public function __construct(
+        private readonly string $name,
+        private readonly string $email,
+        private readonly string $password
+    ) {}
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            name: $data['name'],
+            email: $data['email'],
+            password: $data['password']
+        );
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+}
