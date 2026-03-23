e6eefade0f37479600e249b1f6b5778b6efb7acf
Denys Borovyk <borovikdenis748@gmail.com>
2026-03-23 18:25:02 +0200

    moved custom configs to custom folder

--- config/pagination.php
+++
@@ -1,5 +0,0 @@
-<?php declare(strict_types=1);
-
-return [
-    'admin_per_page' => 12,
-];
--- config/rate_limiter.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-
-return [
-    'api_authenticated' => 5,
-    'api_guest' => 30,
-    'login' => 5,
-];
