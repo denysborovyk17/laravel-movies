87d8711b7d6554771bc591286ff94d22797c9ad2
Denys Borovyk <borovikdenis748@gmail.com>
2026-03-25 12:19:32 +0200

    change system of search in index method using by MovieSearchDto and refactored listPublic method

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\DTO\MovieSearchDto;
 use App\Models\Movie;
 use App\Services\Interfaces\MovieServiceInterface;
 use Illuminate\Http\Request;
@@ -15,7 +16,12 @@ class MovieController extends Controller
 
     public function index(Request $request): View
     {
-        $movies = $this->movieService->listPublic($request->input('search'), 12);
+        $search = new MovieSearchDto(
+            search: $request->input('search'),
+            perPage: config('custom.pagination.public_per_page')
+        );
+    
+        $movies = $this->movieService->listPublic($search);
 
         return view('movies.index', compact('movies'));
     }
