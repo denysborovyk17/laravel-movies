048c05cac669d283d50c78f1d4965e4cd0c53602
Denys Borovyk <borovikdenis748@gmail.com>
2026-03-26 22:47:15 +0200

    created MovieListResource instead of MovieCollection

---
+++ app/Http/Resources/MovieListResource.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class MovieListResource extends ResourceCollection
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'data' => $this->collection->map(fn($movie) => [
+                'id' => $movie->id,
+                'title' => $movie->title,
+                'director' => $movie->director,
+                'genre' => $movie->genre,
+                'rating' => $movie->rating
+            ]),
+        ];
+    }
+}
