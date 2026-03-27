83fd7f2bea09fcacec700fa03bdffe1674779b46
Denys Borovyk <borovikdenis748@gmail.com>
2026-03-27 19:34:27 +0200

    adapted for MailService what using in the constructor

--- app/Listeners/SendWelcomeNotification.php
+++ app/Listeners/SendWelcomeNotification.php
@@ -3,26 +3,24 @@
 namespace App\Listeners;
 
 use App\Events\UserRegistered;
-use App\Mail\WelcomeMail;
+use App\Services\Interfaces\MailServiceInterface;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Support\Facades\Mail;
 
 class SendWelcomeNotification implements ShouldQueue
 {
     /**
      * Create the event listener.
      */
-    public function __construct()
-    {
-        //
-    }
+    public function __construct(
+        private readonly MailServiceInterface $mailService
+    ) {}
 
     /**
      * Handle the event.
      */
     public function handle(UserRegistered $event): void
     {
-        Mail::to($event->user->email)->send(new WelcomeMail($event->user));
+        $this->mailService->sendWelcomeEmail($event->userId);
     }
 }
