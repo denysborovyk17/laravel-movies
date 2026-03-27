ce2f9bcfb4d50a0e0da0244515b74ee84a6301b4
Denys Borovyk <borovikdenis748@gmail.com>
2026-03-27 14:13:48 +0200

    created view for sending registration email

---
+++ resources/views/email/welcome-mail.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome Registration Page</title>
+</head>
+<body>
+    <h1>Welcome, {{ $user->name }}!</h1>
+    <p>Thanks for registration on our site about movies!</p>
+</body>
+</html>
