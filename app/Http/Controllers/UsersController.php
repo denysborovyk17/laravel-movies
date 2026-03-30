3c433677ba29bb4b6b3e217b0c36bf34c03bc44c
Denys Borovyk <borovikdenis748@gmail.com>
2026-03-30 23:05:23 +0300

    create UsersController for downloads all users from DB in .xlsx using by UsersExport

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Exports\UsersExport;
+use Maatwebsite\Excel\Facades\Excel;
+
+class UsersController extends Controller
+{
+    public function export()
+    {
+        return Excel::download(new UsersExport, 'users.xlsx');
+    }
+}
