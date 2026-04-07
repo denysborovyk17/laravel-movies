dd31ae8e291b791c06510d2badf2bd3a3d00ad3f
Denys Borovyk <borovikdenis748@gmail.com>
2026-04-07 18:27:47 +0300

    added conditions for export users and headings for columns

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -1,18 +1,35 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace App\Exports;
 
 use App\Models\User;
+use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class UsersExport implements FromCollection, ShouldAutoSize
+class UsersExport implements FromCollection, ShouldAutoSize, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    public function collection(): Collection
     {
-        return User::all();
+        return User::where('name', 'like', 'A%')
+            ->orderBy('created_at', 'asc')
+            ->limit(10)
+            ->get();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'id',
+            'name',
+            'email',
+            'email_verified_at',
+            'created_at',
+            'updated_at'
+        ];
     }
 }
