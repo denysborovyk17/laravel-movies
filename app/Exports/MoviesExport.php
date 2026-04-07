8e2987d3276a39e3e102bdeedb8cd6f15d00b52b
Denys Borovyk <borovikdenis748@gmail.com>
2026-04-07 18:30:07 +0300

    create export for movies with conditions to rating and added headings for columns

---
+++ app/Exports/MoviesExport.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace App\Exports;
+
+use App\Models\Movie;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class MoviesExport implements FromCollection, ShouldAutoSize, WithHeadings
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection(): Collection
+    {
+        return Movie::with('director')
+            ->where('rating', '>', 7)
+            ->orderBy('rating', 'desc')
+            ->get();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'id',
+            'title',
+            'director',
+            'image',
+            'slug',
+            'description',
+            'year',
+            'genre',
+            'rating',
+            'status'
+        ];
+    }
+}
