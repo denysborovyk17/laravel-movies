a64e982e081ad8924c298e871fd2a1ce12a06869
Denys Borovyk <borovikdenis748@gmail.com>
2026-04-09 23:08:35 +0300

    rename method delete to destroy

--- app/Policies/MoviePolicy.php
+++ app/Policies/MoviePolicy.php
@@ -6,17 +6,17 @@ use App\Models\{User, Movie};
 
 class MoviePolicy
 {
-    public function create(User $user): bool
-    {
-        return $user->isAdmin();
-    }
-
     public function update(User $user, Movie $movie): bool
     {
         return $user->isAdmin();
     }
 
-    public function delete(User $user, Movie $movie): bool
+    public function destroy(User $user, Movie $movie): bool
+    {
+        return $user->isAdmin();
+    }
+
+    public function restore(User $user, Movie $movie): bool
     {
         return $user->isAdmin();
     }
