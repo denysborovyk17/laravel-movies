027d17fd5de867615d9a3fed73f5001b39f55101
Denys Borovyk <borovikdenis748@gmail.com>
2026-04-09 12:21:50 +0300

    renamed MovieListResource to MovieCollection

--- app/Http/Resources/MovieCollection.php
+++ app/Http/Resources/MovieCollection.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
-class MovieListResource extends ResourceCollection
+class MovieCollection extends ResourceCollection
 {
     public function toArray(Request $request): array
     {
